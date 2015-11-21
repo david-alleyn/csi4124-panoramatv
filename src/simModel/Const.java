6350b719f989d737e6f401695b6d467daaef1a81
rmush029 <rmush029@uottawa.ca>
2015-11-21 12:50:00 -0500

    update constants.

--- src/simModel/Const.java
+++ src/simModel/Const.java
@@ -1,9 +1,10 @@
 package simModel;
 
-class Const 
+public class Const 
 {
 	/* Constants */
 	// Define constants as static
 	// Example: protected final static double realConstant = 8.0;
+	public static int ELECTRONIC_ON_SIZE = 1;
 
 }
