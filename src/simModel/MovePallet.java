057b2eb8d2137a47e52b5676ce4b134e91277cad
deadly4u <deadly4u@shcsclan.com>
2015-11-21 11:28:09 -0500

    -absModJ has been updated to latest version. -Project files updated to reflect library change -All base classes for our work have been added to the project

---
+++ src/simModel/MovePallet.java
@@ -0,0 +1,25 @@
+package simModel;
+
+import simulationModelling.ConditionalActivity;
+
+public class MovePallet extends ConditionalActivity {
+
+	@Override
+	protected double duration() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startingEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void terminatingEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/simModel/RepairEquipment.java
@@ -0,0 +1,31 @@
+package simModel;
+
+import simulationModelling.ScheduledActivity;
+
+public class RepairEquipment extends ScheduledActivity {
+
+	@Override
+	protected double timeSequence() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected double duration() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startingEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void terminatingEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
