2c86e5d6484eefd19ffb0860dca1e4e32318093c
rmush029 <rmush029@uottawa.ca>
2015-11-28 19:12:20 -0500

    manualNode done from the delivarable doc.

--- src/simModel/ManualNode.java
+++ src/simModel/ManualNode.java
@@ -1,5 +1,21 @@
 package simModel;
+/**
+ * The 8 manual segments in the system.  
+ * OP10, Rework, OP60, OP40a, OP40b, OP40c, OP40d, OP40e are the eight identifiers of the segment
+ * @author mush
+ */
 
 public class ManualNode {
+	/**
+	 * Set to TRUE when the Operator is processing a TV and FALSE otherwise
+	 */
+	private Boolean busy;
+	
+	public void setBusy(Boolean busy){
+		this.busy = busy;
+	}
+	public Boolean getBusy(){
+		return this.busy;
+	}
 
 }
