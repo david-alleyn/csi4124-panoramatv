d3d87243dc8008d5a4d2d24514ace03f981638a9
rmush029 <rmush029@uottawa.ca>
2015-11-29 20:49:05 -0500

    pallets updated

--- src/simModel/Pallet.java
+++ src/simModel/Pallet.java
@@ -11,4 +11,8 @@ public class Pallet {
 	Boolean inMotion = false;
 	Boolean finishedProcessing = false;
 	Boolean moveRework = false;
+	/** The current position of the pallet on the conveyor segment which it occupies. currentPosition = 0 */
+	int currPosition = 0; 
+	/** The current conveyor that the pallet is occupying */
+	int currConveyor = -1;
 }
