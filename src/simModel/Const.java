9b92f2783ec8fb6f9625976368da346f4edee15e
deadly4u <deadly4u@shcsclan.com>
2015-12-03 21:37:11 -0500

    OOPS LOL again

--- src/simModel/Const.java
+++ src/simModel/Const.java
@@ -31,22 +31,22 @@ public class Const
 	/**
 	 * AutoNode ID.
 	 */
-	final static int OP20   = 1;
-	final static int OP30   = 2;
-	final static int TEST   = 3;
-	final static int OP50   = 4;
+	final static int OP20   = 0;
+	final static int OP30   = 1;
+	final static int TEST   = 2;
+	final static int OP50   = 3;
 	
 	/**
 	 * ManualNode ID.
 	 */
-	final static int OP10   = 1;
-	final static int OP40a   = 2;
-	final static int OP40b   = 3;
-	final static int OP40c   = 4;
-	final static int OP40d   = 5;
-	final static int OP40e   = 6;
-	final static int REWORK = 7;
-	final static int OP60 = 8;
+	final static int OP10   = 0;
+	final static int OP40a   = 1;
+	final static int OP40b   = 2;
+	final static int OP40c   = 3;
+	final static int OP40d   = 4;
+	final static int OP40e   = 5;
+	final static int REWORK = 6;
+	final static int OP60 = 7;
 	
 	final static int[] CAPACITY = 
 		{
